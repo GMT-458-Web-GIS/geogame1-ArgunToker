848c1da5b68ab4cb164eced97221f0e317dcb92e
ArgunToker <119879519+ArgunToker@users.noreply.github.com>
2024-12-13 09:02:16 +0300

    restart button.js

---
+++ restart button.js
@@ -0,0 +1,7 @@
+Restart Button Click:
+
+Resets the game to its initial state.
+Returns the user to the mode selection screen.
+javascript
+Copy code
+restartButton.addEventListener("click", showModeSelection);
